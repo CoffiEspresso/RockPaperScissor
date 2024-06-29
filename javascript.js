e3a4c4fd6c085e4e6ea4dfd5b7999af82358cf11
Coffi <160792583+CoffiEspresso@users.noreply.github.com>
2024-06-29 10:45:03 +0200

    first GUI version

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
-const choices = ["rock", "paper", "scissor"];
-const totalRounds = parseInt(prompt("How many rounds?"));
+const choices = ["rock", "paper", "scissors"];
+
+//const totalRounds = parseInt(prompt("How many rounds?"));
 
 let humanScore = 0;
 let computerScore = 0;
+let playerScoreTag = document.querySelector("#playerPoints");
+let cpuScoreTag = document.querySelector("#cpuPoints");
 
-playGame(totalRounds);
+
+//playGame(totalRounds);
 
 
 
@@ -12,10 +16,11 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]; // I want a random number between 0 and 2
 }
 
-function getHumanChoice() {
-    let answer = prompt("Fai la tua mossa!");
+/* function getHumanChoice(answer) {
+    //let answer = prompt("Fai la tua mossa!");
     return answer.toLowerCase().trim();
 }
+ */
 
 function playRound(humanChoice, computerChoice) {
 
@@ -26,29 +31,44 @@ function playRound(humanChoice, computerChoice) {
     else {
         switch (humanChoice) {
             case 'rock': computerChoice == 'paper' ? cpuWins() : playerWins(); break;
-            case 'paper': computerChoice == 'scissor' ? cpuWins() : playerWins(); break;
-            case 'scissor': computerChoice == 'rock' ? cpuWins() : playerWins(); break;
+            case 'paper': computerChoice == 'scissors' ? cpuWins() : playerWins(); break;
+            case 'scissors': computerChoice == 'rock' ? cpuWins() : playerWins(); break;
         }
     }
+    if(humanScore == 5 || computerScore == 5) declareWinner();
+}
+
+function declareWinner(){
+    humanScore > computerScore ? alert("You Win! \n Game will be restarted") : alert("Cpu Win! \n Game will be restarted");
+    restart();
+}
+
+function restart(){
+    document.querySelectorAll('span').forEach (span => {span.textContent = '0';});
+    humanScore =0;
+    computerScore =0;
+
 }
 
 function playerWins() {
-    console.log("You Win!");
     humanScore++;
+    playerScoreTag.textContent = humanScore;
 }
 function cpuWins() {
-    console.log("CPU Win!");
     computerScore++;
-}
+    cpuScoreTag.textContent = computerScore;
+} 
 
-function playGame(rounds) {
+/* function playGame(rounds) {
 
     for (let i = 0; i < rounds; i++) {
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+        }
+        //QUI VA PLAYROUND
+        console.log(humanScore);
+        console.log(computerScore);
+        } */
 
-    console.log(humanScore);
-    console.log(computerScore);
-}
+let btnList = document.querySelectorAll("button");
+btnList.forEach(btn => {btn.addEventListener("click", (e) => { playRound(e.target.id, getComputerChoice());})}); //Not much readable, but useful for learning
